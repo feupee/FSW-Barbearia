2c369344f168a94c228ea2c897abb12ec1fd6869
Felipe S. Silva <felipe.silva7@ufu.br>
2025-09-27 03:32:36 -0300

    feat: finish home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon } from "lucide-react"
+import { EyeIcon, FootprintsIcon, SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
 import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
@@ -16,32 +16,75 @@ const Home = async () => {
   //Chamar meu banco de dados
   //Pegando as barbearias do banco
   const barbershop = await db.barbershop.findMany({})
+
+  //Pegando as barbearias populares
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
+
   return (
     <div className="">
       <Header />
       <div className="p-7">
         <h2 className="text-xl font-bold">Olá, Felipe</h2>
         <p>Sexta-feira, 26 de Setembro</p>
-        <div className="mt-6 flex items-center gap-8">
+        <div className="mt-6 flex items-center gap-4">
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
-        {/* <div className="flex items-center justify-between mt-6">
-          <Button variant="secondary">
-            <Image alt="Cabelo" src="/heroicons_scissors-20-solid.png" height={18} width={120} />
-              Cabelo
-          </Button>
-          <Button variant="secondary">
-              Barba
-          </Button>
-          <Button variant="secondary">
-              Acabamento
-          </Button>
-        </div>*/}
 
-        {/* agende nos melhores */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button variant="secondary">
+            <Image
+              alt="Cabelo"
+              src="/heroicons_scissors-20-solid.png"
+              height={16}
+              width={16}
+            />
+            Cabelo
+          </Button>
+          <Button variant="secondary">
+            <Image
+              alt="Cabelo"
+              src="/mdi_mustache.png"
+              height={16}
+              width={16}
+            />
+            Barba
+          </Button>
+          <Button variant="secondary">
+            <Image
+              alt="Navalha"
+              src="/mdi_razor-double-edge.png"
+              height={16}
+              width={16}
+            />
+            Acabamento
+          </Button>
+          <Button variant="secondary">
+            <FootprintsIcon size={16} />
+            Pézinho
+          </Button>
+          <Button variant="secondary">
+            <EyeIcon size={16}/>
+            Sobrancelha
+          </Button>
+          <Button variant="secondary">
+            <Image
+              alt="Navalha"
+              src="/mdi_razor-double-edge.png"
+              height={16}
+              width={16}
+            />
+            Acabamento
+          </Button>
+        </div>
+
+        {/* agende nos melhores - IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW-Barber"
@@ -76,16 +119,36 @@ const Home = async () => {
             </div>
           </CardContent>
         </Card>
+
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
         {/*renderizando as barbearias*/}
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
           {barbershop.map((barbershop) => (
-          <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-        ))}
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        {/*renderizando as barbearias*/}
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
         </div>
       </div>
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2025 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
