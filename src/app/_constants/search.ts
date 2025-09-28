a1826e59cd5e6180b673edd1779c8fd9890b7c0d
Felipe S. Silva <felipe.silva7@ufu.br>
2025-09-28 13:39:36 -0300

    feat: add BookingItem component

---
+++ src/app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOptions {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOptions[] = [
+  {
+    imageUrl: "/tesoura.png",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/bigode.png",
+    title: "Bigode",
+  },
+  {
+    imageUrl: "/gilete.png",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/shampoo.png",
+    title: "Hidratação",
+  },
+  {
+    imageUrl: "/sobrancelha.png",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/toalha.png",
+    title: "Massagem",
+  },
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { EyeIcon, FootprintsIcon, SearchIcon } from "lucide-react"
+import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
 import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const name = "test"
 
@@ -38,50 +38,17 @@ const Home = async () => {
         </div>
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button variant="secondary">
-            <Image
-              alt="Cabelo"
-              src="/heroicons_scissors-20-solid.png"
-              height={16}
-              width={16}
-            />
-            Cabelo
-          </Button>
-          <Button variant="secondary">
-            <Image
-              alt="Cabelo"
-              src="/mdi_mustache.png"
-              height={16}
-              width={16}
-            />
-            Barba
-          </Button>
-          <Button variant="secondary">
-            <Image
-              alt="Navalha"
-              src="/mdi_razor-double-edge.png"
-              height={16}
-              width={16}
-            />
-            Acabamento
-          </Button>
-          <Button variant="secondary">
-            <FootprintsIcon size={16} />
-            Pézinho
-          </Button>
-          <Button variant="secondary">
-            <EyeIcon size={16}/>
-            Sobrancelha
-          </Button>
-          <Button variant="secondary">
-            <Image
-              alt="Navalha"
-              src="/mdi_razor-double-edge.png"
-              height={16}
-              width={16}
-            />
-            Acabamento
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button variant="secondary" key={option.title}>
+              <Image
+                alt= {option.title}
+                src={option.imageUrl}
+                height={16}
+                width={16}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         {/* agende nos melhores - IMAGEM */}
@@ -95,30 +62,7 @@ const Home = async () => {
         </div>
 
         {/* Agendamento */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card className="">
-          <CardContent className="flex justify-between p-0">
-            {/* Esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Setembro</p>
-              <p className="pb-1 text-3xl">26</p>
-              <p className="text-sm">20:09</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem/>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
