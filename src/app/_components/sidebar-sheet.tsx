160e483a230eb6f39362225a9aaed57854ffa075
Felipe S. Silva <felipe.silva7@ufu.br>
2025-09-30 14:18:02 -0300

    feat: remove signout withou account logged

--- src/app/_components/sidebar-sheet.tsx
+++ src/app/_components/sidebar-sheet.tsx
@@ -9,22 +9,12 @@ import { quickSearchOptions } from "../_constants/search"
 import { Avatar } from "./ui/avatar"
 import { AvatarImage } from "@radix-ui/react-avatar"
 import Link from "next/link"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog"
-import { signIn, signOut, useSession } from "next-auth/react"
-import { sign } from "crypto"
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
+import { signOut, useSession } from "next-auth/react"
 import SignInDialog from "./sign-in-dialog"
 
 const SideBarSheet = () => {
-
-  const {data} = useSession()
-  const handleLoginWithgoogleClick = () => signIn("google")
+  const { data } = useSession()
   const handleLogoutClick = () => signOut()
   return (
     <SheetContent className="overflow-y-auto">
@@ -33,36 +23,31 @@ const SideBarSheet = () => {
       </SheetHeader>
 
       <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
-        
         {/*Se eu tiver um usuário logado com a conta do google ele mostra as informações
         dele, caso contrário mostra informações padrões para realizar login */}
         {data?.user ? (
           <div className="flex items-center gap-2">
-          <Avatar>
-            <AvatarImage
-              src={data?.user?.image ?? ""}
-              alt="Avatar"
-            />
-          </Avatar>
-          <div>
-            <p className="font-bold">{data?.user?.name}</p>
-            <p className="text-xs text-gray-400">{data?.user?.email}</p>
-          </div>
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ""} alt="Avatar" />
+            </Avatar>
+            <div>
+              <p className="font-bold">{data?.user?.name}</p>
+              <p className="text-xs text-gray-400">{data?.user?.email}</p>
+            </div>
           </div>
         ) : (
           <>
-          <h2 className="pt-3 text-lg font-bold">Olá, faça seu login</h2>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button className="ml-auto" variant="default" size="sm">
-              <LogInIcon size="icon" />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="w-[90%]">
-            <SignInDialog />
-            </DialogContent>
-          
-        </Dialog>
+            <h2 className="pt-3 text-lg font-bold">Olá, faça seu login</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="ml-auto" variant="default" size="sm">
+                  <LogInIcon size="icon" />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <SignInDialog />
+              </DialogContent>
+            </Dialog>
           </>
         )}
       </div>
@@ -85,30 +70,36 @@ const SideBarSheet = () => {
         {quickSearchOptions.map((option) => (
           <SheetClose asChild key={option.title}>
             <Button
-            className="justify-start gap-2"
-            variant="ghost"
-            key={option.title}
-            asChild
-          >
-            <Link href={`/barbershops?service=${option.title}`}>
-            <Image
-              alt={option.title}
-              src={option.imageUrl}
-              height={18}
-              width={18}
-            />
-            {option.title}
-            </Link>
-          </Button>
+              className="justify-start gap-2"
+              variant="ghost"
+              key={option.title}
+              asChild
+            >
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  alt={option.title}
+                  src={option.imageUrl}
+                  height={18}
+                  width={18}
+                />
+                {option.title}
+              </Link>
+            </Button>
           </SheetClose>
         ))}
       </div>
-      <div className="flex flex-col gap-2 py-5">
-        <Button className="justify-start gap-2" variant="ghost" onClick={handleLogoutClick}>
-          <LogOutIcon size={18} />
-          Sair da Conta
-        </Button>
-      </div>
+      {data?.user && (
+        <div className="flex flex-col gap-2 py-5">
+          <Button
+            className="justify-start gap-2"
+            variant="ghost"
+            onClick={handleLogoutClick}
+          >
+            <LogOutIcon size={18} />
+            Sair da Conta
+          </Button>
+        </div>
+      )}
     </SheetContent>
   )
 }
